a8e1b1e95717ce1a4191a8506448982ed89f69ee
PrabhuV2003 <prabhuofficial2003@gmail.com>
2025-07-28 11:06:32 +0530

    assets Optimization

--- public/js/countdown-custom.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-    $(function() {
-        $('#defaultCountdown').countdown({
-            until: new Date(2025, 7, 1, 1)
-        }); // year, month, date, hour
-    });
-});
